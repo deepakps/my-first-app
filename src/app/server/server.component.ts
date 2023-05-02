c65f47874ec787b1508bc5f9435419b7f066f496
Deepak Shinde <deepak27shinde@gmail.com>
2023-05-02 12:36:07 +0530

    Section 1:17 - Creating a New Component.

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// Decorators are typescript features that basically enhance JS classes.
+// Decorators are always attached by adding '@' sign before them.
+// Date - 02nd May, 2023.
+@Component({
+    selector: 'app-server',
+    template: './server.component.html'
+})
+export class ServerComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+// Code from this file first gets excuted in an Angular app.
+// Date - 01st May, 2023.
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
+// 'bootstrapModule' is where bootstrap starts Angular app by passing 'AppModule'. 
+// 'AppModule' refers to app.modules.ts file inside src > app folder.
+// Date - 01st May, 2023.
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
