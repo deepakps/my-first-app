01e70e49d0e4ad6ec2cb920552d93bcaeece52d0
Deepak Shinde <deepak27shinde@gmail.com>
2023-05-03 13:33:34 +0530

    Section 2 - Assignment 1: practicing component.

---
+++ src/app/notification-alert/notification-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-alert',
+  templateUrl: './notification-alert.component.html',
+  styleUrls: ['./notification-alert.component.css']
+})
+export class NotificationAlertComponent {
+
+}
