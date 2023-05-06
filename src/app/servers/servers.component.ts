e7c4b4ae4ed16c64a0fa789e732126266d6f0e00
Deepak Shinde <deepak27shinde@gmail.com>
2023-05-06 16:50:28 +0530

    Section 2:26 - String Interpolation.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   // Date - 02nd May, 2023.
-  // selector: 'app-servers', -- selector using tag.
-  // selector: '[app-servers]', -- selector using attribute.
-  selector: '.app-servers',
-  template: `<h4>Components Section : </h4>
-            <app-server />
-            <app-server />`,
+  selector: 'app-servers', // selector using tag.
+  // selector: '[app-servers]', // selector using attribute.
+  // selector: '.app-servers', // selector using class.
+  templateUrl: './servers.component.html',
+  // template: `<h4>Components Section : </h4>
+  //           <app-server />
+  //           <app-server />`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
